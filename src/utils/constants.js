35b676ffec12d41bdf7dabed8ccf911f7a3f2acf
arisu6804 <arisu6804@gmail.com>
2026-06-12 17:17:17 +0530

    feat(utils): add shared constants module

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Shared application constants.
+ */
+module.exports = {
+  API_VERSION: 'v1',
+  SERVICE_NAME: 'yieldvault-backend',
+  // Supported underlying assets for vaults in the mock environment.
+  SUPPORTED_ASSETS: ['USDC', 'XLM', 'EURC'],
+};
