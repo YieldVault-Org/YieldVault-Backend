60ae06971274ab2d4b6692c591e8b4bfe3678ccf
arisu6804 <arisu6804@gmail.com>
2026-06-12 17:12:39 +0530

    feat(middleware): add structured request logger

---
+++ src/middleware/requestLogger.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const logger = require('../utils/logger');
+
+/**
+ * Lightweight request logger that records method, path, status and duration.
+ * Complements morgan with structured, level-aware output.
+ */
+module.exports = function requestLogger(req, res, next) {
+  const start = process.hrtime.bigint();
+
+  res.on('finish', () => {
+    const durationMs = Number(process.hrtime.bigint() - start) / 1e6;
+    logger.info(
+      `${req.method} ${req.originalUrl} ${res.statusCode} ${durationMs.toFixed(1)}ms`
+    );
+  });
+
+  next();
+};
